0fef3aac239afa16d91025f493a1d964d7d57364
divyanshi <contactdiv01@gmail.com>
2024-01-02 22:43:14 +0530

    added new function objectLoop

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
-var a=10;
-function loopDemo(){
-  for(i=1;i<=a;i++){
+var a = 10;
+function loopDemo() {
+  for (i = 1; i <= a; i++) {
     console.log(i);
-    console.log('====================================');
+    console.log("====================================");
   }
 }
 loopDemo();
+
+function objectLoop() {
+  const y = [
+    {
+      name: "Divyanshi",
+      id: 29,
+    },
+    {
+      name: "Naman",
+      id: 1,
+    },
+  ];
+  y.forEach((element) => {
+    console.log(element.name + "  " + element.id);
+    console.log("====================================");
+  });
+}
+objectLoop();
